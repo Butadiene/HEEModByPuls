7a92c11bf035ff69b4d3216f13c4c5232d679e45
Butadiene <najimi0510@gmail.com>
2020-12-22 02:06:37 +0900

    cmake sucess!!

---
+++ build/src/CMakeFiles/HeeModandGeoMagPuls.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HeeModandGeoMagPuls.dir/Account.cpp.o"
+  "libHeeModandGeoMagPuls.pdb"
+  "libHeeModandGeoMagPuls.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HeeModandGeoMagPuls.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
