a286139cf74b805785e779312dc1701149577605
Butadiene <najimi0510@gmail.com>
2021-04-17 19:58:24 +0900

    opposite

--- graphs/graphScript/test.py
+++ graphs/graphScript/test.py
@@ -9,33 +9,35 @@ import numpy as np
 # グラフとして描画するデータ
 fig, ax = plt.subplots(1, 1)
 
-m_num = 20
+m_num = 80
 
-data = pd.read_csv('data/testdatav2/test001_20_2.csv',header = None)
-plt.imshow(data, cmap=plt.cm.get_cmap("jet"), interpolation='nearest',aspect = 2000, extent=[0,6400*6*2*3.14/m_num,0.0036*2000,0])
+data = pd.read_csv('data/testdatav2/test001_80_270.csv',header = None)
+plt.imshow(data, cmap=plt.cm.get_cmap("jet"), interpolation='nearest',aspect = 500, extent=[0,6400*6*2*3.14/m_num,0.0036*2000,0])
 
 ax.set_xlabel('position[km]')
 ax.set_ylabel('Time[T=ULFWavePeriod]')
 
 
 x = np.arange(0, 6400*6*2*3.14/m_num, 2)
-y = x*0+2.5
-ax.plot(x, y, color=(1,0,1), linewidth=6)
+y = x*0+3.05
+#ax.plot(x, y, color=(1,0,0), linewidth=4)
 
-#y = x*0+3
-#ax.plot(x, y, color=(1,0,1), linewidth=2)
+y = x*0+3.35
+#ax.plot(x, y, color=(0,1,0), linewidth=4)
 
+y = x*0+4.05
+#ax.plot(x, y, color=(0,1,1), linewidth=4)
 
 
 N = 10 # 線の数
 x = np.linspace(-1, 1, N)
 
 # 線のリスト : [(x0, y0), (x1, y1)]が1つの線
-lines = [[(0, i), (6400*6*2*3.14/m_num, (i+1))] for i in range(N)]
-lc = mc.LineCollection(lines, colors=(1,1,1), linewidths=2)
-ax.add_collection(lc)
+lines = [[(0, (i+1)), (6400*6*2*3.14/m_num, i)] for i in range(N)]
+lc = mc.LineCollection(lines, colors=(1,1,1), linewidths=1,linestyles = "dotted")
+#ax.add_collection(lc)
 
-fig.text(1, 0, 'WhiteLine:ULF Waves Bottom', ha='right')
+fig.text(1, 0, 'WhiteLine:ULF Waves Top', ha='right')
 plt.colorbar()
 
 
--- graphs/graphScript/test2.py
+++ graphs/graphScript/test2.py
@@ -9,10 +9,10 @@ import numpy as np
 # グラフとして描画するデータ
 fig, ax1 = plt.subplots(1, 1)
 
-m_num = 20
+m_num = 80
 focusTime = 2.5
 
-data = pd.read_csv('data/testdatav2/test001_20_2.csv',header=None)
+data = pd.read_csv('data/testdatav2/test001_80_270.csv',header=None)
 datay = data.iloc[int(focusTime/(0.0108))]
 
 print(datay)
@@ -22,13 +22,14 @@ len = 6400*6*2*3.14/m_num
 x = np.arange(0,len,len/128)
 ax1.set_xlabel('position[km]')
 ax1.set_ylabel('density')
-ax1.plot(x,datay,color = (0,0,1),label = "density")
+#ax1.set_ylim(0,5)
+ax1.plot(x,datay,color = (0,0,1),label = "density", linewidth=4)
 
 ax2 = ax1.twinx()
-data2 = -1*pd.read_csv('data/testdatav2/test001_20_sinv2.csv',header=None)
+data2 = -1*pd.read_csv('data/testdatav2/test001_80_245_sin.csv',header=None)
 datay2 = data2.iloc[int(focusTime/(0.0108))]
 ax2.set_ylabel('-ULF waves')
-ax2.plot(x, datay2,color=(0,1,0),label = "-ULF waves")
+ax2.plot(x, datay2,color=(0,1,0),label = "-ULF waves", linewidth=4)
 h1, l1 = ax1.get_legend_handles_labels()
 h2, l2 = ax2.get_legend_handles_labels()
 ax1.legend(h1+h2, l1+l2, loc='upper right')
